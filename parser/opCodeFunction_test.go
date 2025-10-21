1935334369532a0372ce6de1e4e924c6358e7ce6
Jérémie abt <abtjeremie@gmail.com>
2025-10-21 11:26:51 +0200

    wip

--- parser/opCodeFunction_test.go
+++ parser/opCodeFunction_test.go
@@ -42,6 +42,307 @@ func Test_handleMov(t *testing.T) {
 				},
 			},
 		},
+		{
+			"move with immediate value",
+			[]byte("MOV EAX, 42"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Name: RegisterEAX,
+						},
+						&ImmediateOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 9,
+							},
+							Value: 42,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move with hex immediate",
+			[]byte("MOV ECX, 0xFF"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Name: RegisterECX,
+						},
+						&ImmediateOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 9,
+							},
+							Value: 0xFF,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move with memory operand",
+			[]byte("MOV EAX, [EBX]"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Name: RegisterEAX,
+						},
+						&MemoryOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 9,
+							},
+							Base: RegisterEBX,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move with memory offset",
+			[]byte("MOV EDX, [EBP+8]"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Name: RegisterEDX,
+						},
+						&MemoryOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 9,
+							},
+							Base:   RegisterEBP,
+							Offset: 8,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move to memory from register",
+			[]byte("MOV [ESI], EAX"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&MemoryOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Base: RegisterESI,
+						},
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 11,
+							},
+							Name: RegisterEAX,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move with negative offset",
+			[]byte("MOV EAX, [EBP-4]"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Name: RegisterEAX,
+						},
+						&MemoryOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 9,
+							},
+							Base:   RegisterEBP,
+							Offset: -4,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move with SIB addressing",
+			[]byte("MOV EAX, [EBX+ECX*4]"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Name: RegisterEAX,
+						},
+						&MemoryOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 9,
+							},
+							Base:  RegisterEBX,
+							Index: RegisterECX,
+							Scale: 4,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move with extra whitespace",
+			[]byte("MOV    EAX,    EBX"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 7,
+							},
+							Name: RegisterEAX,
+						},
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 15,
+							},
+							Name: RegisterEBX,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move lowercase",
+			[]byte("mov eax, ebx"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Name: RegisterEAX,
+						},
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 9,
+							},
+							Name: RegisterEBX,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move with zero immediate",
+			[]byte("MOV ESI, 0"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Name: RegisterESI,
+						},
+						&ImmediateOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 9,
+							},
+							Value: 0,
+						},
+					},
+				},
+			},
+		},
+		{
+			"move with large immediate",
+			[]byte("MOV EDI, 0xFFFFFFFF"),
+			[]Node{
+				&Instruction{
+					Column: 0,
+					Line:   0,
+					OpCode: OpCodeMOV,
+					Operands: []Node{
+						&RegisterOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 4,
+							},
+							Name: RegisterEDI,
+						},
+						&ImmediateOperand{
+							BaseOperand: BaseOperand{
+								Line:   0,
+								Column: 9,
+							},
+							Value: 0xFFFFFFFF,
+						},
+					},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		lexerObj := lexer.NewLexer()
